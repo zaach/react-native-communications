6136fb650294d59c678b0defd7949821880c6010
Matthew Knight <matthewknight@Matthews-Air.home>
2016-12-11 22:16:30 +0000

    Added a workaround to prevent crash when cancel call using telprompt scheme on iOS

--- AKCommunications.js
+++ AKCommunications.js
@@ -158,9 +158,18 @@ const LaunchURL = function(url) {
 		if(!supported) {
 			console.log('Can\'t handle url: ' + url);
 		} else {
-			return Linking.openURL(url);
+			Linking.openURL(url)
+			.catch(err => {
+				if(url.includes('telprompt')) {
+					// telprompt was cancelled and Linking openURL method sees this as an error
+					// it is not a true error so ignore it to prevent apps crashing
+					// see https://github.com/anarchicknight/react-native-communications/issues/39
+				} else {
+					console.warn('openURL error', err)
+				}
+			});
 		}
-	}).catch(err => console.error('An unexpected error happened', err));
+	}).catch(err => console.warn('An unexpected error happened', err));
 };
 
 const getValidArgumentsFromArray = function(array, type) {
